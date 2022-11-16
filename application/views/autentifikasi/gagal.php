de04dce25c89d37c954e26b8abe8dab61155c223
yahyamaula <maulanayahya624@gmail.com>
2022-11-16 12:28:59 +0700

    tugas 10

--- application/views/autentifikasi/gagal.php
+++ application/views/autentifikasi/gagal.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initialscale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initialscale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Gagal Aktifasi Akun </title>
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/'); ?>vendor/fontawesomefree/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,30
-0i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendor/fontawesomefree/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
 </head>
@@ -28,30 +27,30 @@
                     <!-- 404 Error Text -->
                     <div class="text-center">
                         <?= $this->session->flashdata('pesan'); ?>
-                        <a href="<?= base_url('autentifikasi'); ?>" class="btn btn-secondar y ">&larr; Close </a>
+                        <a href="<?= base_url('autentifikasi'); ?>" class="btn btn-secondary ">&larr; Close </a>
                     </div>
-                    </d iv>
-                    <!-- /.contai n er-fluid -->
                 </div>
-                <!--End of Main Content -->
+                <!-- /.contai n er-fluid -->
             </div>
-            <!--End of Content Wrapper -->
+            <!--End of Main Content -->
         </div>
-        <!--End of Page Wrap per -->
-        <!--Scroll to Top Button -->
-        <a class="scroll-to-top rounded" href="#page-top"><i class="fas fa-angle-up"></i></a>
-        <!-- Bootstrap core JavaScript -->
-        <script src="<?= base_url('assets/');
-                        ?>vendor/jquery/jquery.min .js"></script>
-        <script src="<?= base_url('assets/');
-                        ?>vendor/bootstrap/js/bootstrap.bundle.min.j s"></script>
-        <!-- Core plugin JavaScript -->
-        <script src="<?= base_url('assets/'); ?>vendor/jqueryeasing/jquery.easing.min.j s"></script>
-        <!-- Custom scripts for all pages-->
-        <script src="<?= base_url('assets/'); ?>js/sb-admin2.min.js"></script>
-        <script>
-            $('.alert-message').alert().delay(3500).slideUp('slow');
-        </script>
+        <!--End of Content Wrapper -->
+    </div>
+    <!--End of Page Wrap per -->
+    <!--Scroll to Top Button -->
+    <a class="scroll-to-top rounded" href="#page-top"><i class="fas fa-angle-up"></i></a>
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js');
+                    ?>"></script>
+    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js');
+                    ?>"></script>
+    <!-- Core plugin JavaScript -->
+    <script src="<?= base_url('assets/>vendor/jqueryeasing/jquery.easing.min.js'); ?>"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="<?= base_url('assets/js/sb-admin2.min.js'); ?>"></script>
+    <script>
+        $('.alert-message').alert().delay(3500).slideUp('slow');
+    </script>
 </body>
 
 </html>
--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -14,36 +14,23 @@
                                 <?= $this->session->flashdata('pesan'); ?>
                                 <form class="user" method="post" action="<?= base_url('autentifikasi'); ?>">
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" value="<?=
-                                                                                                            set_value('email'); ?>" id="email" placeholder="Masukkan Alamat
-Email" name="email">
-                                        <?= form_error(
-                                            'email',
-                                            '<small class="text-danger pl-3">',
-                                            '</small>'
-                                        ); ?>
+                                        <input type="text" class="form-control form-control-user" value="<?= set_value('email'); ?>" id="email" placeholder="Masukkan Alamat Email" name="email">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
-                                        <?= form_error(
-                                            'password',
-                                            '<small class="text-danger pl-3">',
-                                            '</small>'
-                                        ); ?>
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
-                                    <button type="submit" class="btn
-btn-primary btn-user btn-block">
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
                                         Masuk
                                     </button>
                                 </form>
                                 <hr>
                                 <div class="text-center">
-                                    <a class="small" href="<?=
-                                                            base_url('autentifikasi/lupaPassword'); ?>">Lupa Password?</a>
+                                    <a class="small" href="<?= base_url('autentifikasi/lupaPassword'); ?>">Lupa Password?</a>
                                 </div>
                                 <div class="text-center">
-                                    <a class="small" href="<?=
-                                                            base_url('autentifikasi/registrasi'); ?>">Daftar Member!</a>
+                                    <a class="small" href="<?= base_url('autentifikasi/registrasi'); ?>">Daftar Member!</a>
                                 </div>
                             </div>
                         </div>
